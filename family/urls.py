310b38b477f59dbbd8d89e1c3b010f175036a0a6
Javier Pineda <javierpin82@hotmail.com>
2022-10-14 21:25:09 -0500

    se creo views y urls en la aplicacion family

---
+++ family/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from family import views
+
+urlpatterns = [
+    path('ver-familiares/', views.ver_familiares),
+    path('crear-familiar/<str:nombre>/<str:apellido>/<int:edad>/', views.crear_familiar)
+]
--- family/views.py
+++ family/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
+from datetime import datetime
+from family.models import Familiar
+from django.template import Context, Template, loader
 from django.shortcuts import render
 
-# Create your views here.
+def crear_familiar(request, nombre, apellido, edad):
+    
+    familiar = Familiar(nombre=nombre, apellido=apellido, edad=edad, fecha=datetime.now())
+    familiar.save()
+   
+    return render(request, 'crear_familiar.html', {'familiar':familiar})
+
+def ver_familiares(request):
+    
+    familiares = Familiar.objects.all()
+
+    return render(request, 'ver_familiares.html', {'familiares':familiares})
